1bcfbb368419d17f576720ace921484a957110b1
black <1358244533@qq.com>
2025-09-29 21:50:00 +0800

    增加 lmdb、rocksdb 测试案例

--- lmdb_learn/xmake.lua
+++ lmdb_learn/xmake.lua
@@ -1,8 +1,8 @@
 local dir_path = path.relative(os.curdir(), os.projectdir())
 
-add_requires("rocksdb", {system = false, configs = {static = true}, debug = is_mode("debug")})
+add_requires("lmdb", {system = false, configs = {static = true}, debug = is_mode("debug")})
 
 target(dir_path)
     set_kind("binary")
     add_files("*.c")
-    add_packages("rocksdb")
+    add_packages("lmdb")
